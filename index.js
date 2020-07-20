a5aa932fed2803b8991f6f519cfc8fdc228a6d59
Nicole <nicoleshek315@gmail.com>
2020-07-20 12:43:41 +0200

    added jquery and scroll effect

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".about-nav").click(function (e) {
+        e.preventDefault();
+        var aid = $(this).attr("href");
+        $('html,body').animate({
+            scrollTop: $(aid).offset().top
+        }, 'slow');
+    });
+});
